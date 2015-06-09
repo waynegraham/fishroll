0b77cc809e0d43a745e09b4b4f68a0b839f9ccf6
Wayne Graham <wayne.graham@virginia.edu>
2015-06-08 20:25:50 -0400

    use expanded version for debugging

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -201,5 +201,54 @@ document.addEventListener("DOMContentLoaded", function() {
 /*! Navigator Getusermedia - v0.1.0 - 3/9/2012
 * https://github.com/rwldrn/navigator.getusermedia
 * Copyright (c) 2012 Rick Waldron <waldron.rick@gmail.com>; Licensed MIT */
-(function(e,t){var i;e.unprefix||(e.URL||(e.URL=e.webkitURL||e.msURL||e.oURL),t.getUserMedia||(t.getUserMedia=t.webkitGetUserMedia||t.mozGetUserMedia||t.msGetUserMedia)),i=t.getUserMedia,t.getUserMedia=i?function(a,r,U){i.call(t,a,function(t){var i;t.label&&1===t.readyState&&(i=e.URL.createObjectURL(t)),(t.createObjectURL||void 0!==t.currentTime)&&(i=t),r(i,t)},U||function(){})}:void 0})("object"==typeof window&&window||this,this.navigator||{});
+(function( window, navigator ) {
+  var getUserMedia;
+
+  // 2012-03-08 Inspired by https://gist.github.com/f2ac64ed7fc467ccdfe3
+
+  // If unprefix.js is available, use it.
+  // https://github.com/rwldrn/unprefix.js
+  // Otherwise...
+  if ( !window.unprefix ) {
+    // Thanks to Mike Taylr for typing this
+    // https://gist.github.com/f2ac64ed7fc467ccdfe3
+    // normalize window.URL
+    if ( !window.URL ) {
+      window.URL = window.webkitURL || window.msURL || window.oURL;
+    }
+    // normalize navigator.getUserMedia
+    if ( !navigator.getUserMedia ) {
+      navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    }
+  }
+
+  getUserMedia = navigator.getUserMedia;
+
+  navigator.getUserMedia = getUserMedia ? function( opts, callback, errback ) {
+
+    getUserMedia.call( navigator, opts, function( raw ) {
+      var stream;
+
+      // If the stream is raw (ie. Chrome), cook it.
+      if ( raw.label && raw.readyState === 1 ) {
+        stream = window.URL.createObjectURL( raw );
+      }
+
+      // Opera 12.02 does it like this...
+      // Make Firefox Nightly happy...
+      if ( raw.createObjectURL || raw.currentTime !== undefined ) {
+        stream = raw;
+      }
+
+
+      // This is non-standard, but feels like a
+      // "nice to have" way to handle the mixed-matched
+      // implementations of stream params.
+      // This will be removed when the implementations
+      // are updated.
+      callback( stream, /* non-standard */ raw );
+    }, errback || function() {});
+  } : undefined;
+
+} (typeof window === "object" && window || this, this.navigator || {} ) );
 
